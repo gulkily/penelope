1e7e7003d1713798d7db20d413f480d7327689fb
Ilya Gulko <ilya@ilyagulko.com>
2026-01-12 17:25:10 -0500

    Revert "feat: add playwright test baseline"
    This reverts commit 1dbac7f69e4437af6f35febd6667a4f00a341547.

--- tests/e2e/smoke.spec.js
+++
@@ -1,6 +0,0 @@
-const { test, expect } = require("@playwright/test");
-
-test("dashboard loads", async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle("North Star Dashboard");
-});
